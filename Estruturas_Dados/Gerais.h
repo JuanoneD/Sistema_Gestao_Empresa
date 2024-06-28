873c5d9b099f01e9a303eec1a58211b35c5e0900
juanoned <juandiego.jd42@gmail.com>
2024-06-28 14:32:50 -0300

    main

--- Estruturas_Dados/Gerais.h
+++ Estruturas_Dados/Gerais.h
@@ -1,6 +1,6 @@
-#include "Estruturas_Dados\Ingredientes_estruturas.h"
-#include "Estruturas_Dados\Pedidos_estruturas.h"
-#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //Recebe um buffer == String e o endereço de array e adiciona os valores que existem no buffer no array e retorna o tamanho do array
 int split_ids(char * buffer,int ** array)
@@ -22,20 +22,4 @@ int split_ids(char * buffer,int ** array)
         (*array)[size++] = atoi(word);
     }
     return size;
-}
-
-int main()
-{
-    char word[] = "23,23,23,23,23,23";
-    int * ids = malloc(sizeof(int));
-
-    int size = split_ids(word,&array);
-
-    for(int i =0;i<size;i++)
-    {
-        printf("%i ",pesquisar_id_ingrediente(ids[i]));
-    }
-
-    free(array);
-    
 }
